7b059c7de8c82a11b63f2a2b3b7bb779a61b7b2a
Asipko <avsipko@gmail.com>
2022-09-18 10:00:15 +0300

    initial learn cours

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//вывод содержимого в консоль
+let x = 5
+let y = 7
+let name_ = 0
+// console.log(x + y)
+
+console.log(name_)
+
+//let arr = [1,5,4,7,8,91,1,4,5]
+//let arr2 = [...new Set(arr)]
+//console.log(arr2)
